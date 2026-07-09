fb4de4a920a926f9a1f86402ef06a6acd1ab7d2f
hoummadabderrahmane-oss <hoummadabderrahmane@gmail.com>
2026-07-09 20:04:50 +0100

    Create database and project structure

--- config/database.php
+++ config/database.php
@@ -1,5 +1,4 @@
 <?php
-
 $host = "localhost";
 $dbname = "cms_baladiya";
 $username = "root";
@@ -9,11 +8,13 @@ try {
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
         $username,
-        $password
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]
     );
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
