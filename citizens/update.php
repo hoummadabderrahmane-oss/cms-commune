b8aecab31a7163b3965ee6e19dab3434d64e403a
Abderrahmane Hoummad <hoummadabderrahmane@gmail.com>
2026-07-16 19:19:26 +0000

    [huiop]joopj

--- citizens/update.php
+++ citizens/update.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../config/database.php';
 
 if (empty($_SESSION['admin_id'])) {
     header('Location: ../auth/login.php');
