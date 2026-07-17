4b9be3c46e1ef4d8699b3f0b734b5bbeb5ff755e
Abderrahmane Hoummad <hoummadabderrahmane@gmail.com>
2026-07-17 09:18:17 +0000

    fbdbdskdjjdjdjs

--- citizens/ajouter.php
+++ citizens/ajouter.php
@@ -167,4 +167,4 @@ require_once __DIR__ . '/../includes/header.php';
     </div>
 </div>
 
-<?php require_once __DIR__ . '/../includes/footer.php
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
